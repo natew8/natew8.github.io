c35d0a6d6e0b242ddf89161611e31526ff671462
Nathan Waite <natew821@gmail.com>
2021-02-16 04:06:50 -0700

    Restored Layout

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Footer from "../Footer/Footer";
+import Header from "../Header/Header";
+import "../../styles/index.scss";
+import layoutStyles from "./layout.module.scss";
+
+function Layout(props) {
+  return (
+    <div className={layoutStyles.container}>
+      <Header />
+      <div className={layoutStyles.content}>{props.children}</div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
