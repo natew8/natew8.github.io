823439f988816abc081ff2f241ad136c06a1abda
Nathan Waite <natew821@gmail.com>
2021-02-16 04:04:40 -0700

    Restored Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,43 @@
+import { Link } from "gatsby";
+import AniLink from "gatsby-plugin-transition-link/AniLink";
+import React, { useState } from "react";
+import headerStyle from "./header.module.scss";
+import Menu from "./Menu";
+
+function Header(props) {
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <header className={headerStyle.header}>
+      <nav className={headerStyle.nav}>
+        <h1>
+          <AniLink
+            cover
+            direction="down"
+            bg="#1b1924"
+            style={{ textDecoration: "none" }}
+            className={headerStyle.logo}
+            to="/"
+          >
+            NW
+          </AniLink>
+        </h1>
+        <div>
+          <Menu menu={menu} closeMenu={setMenu} />
+          <h1
+            onClick={() => setMenu(!menu)}
+            className={
+              !menu ? headerStyle.burgerMenu : headerStyle.burgerMenuActive
+            }
+          >
+            <div className={headerStyle.burgerBarOne}></div>
+            <div className={headerStyle.burgerBarTwo}></div>
+            <div className={headerStyle.burgerBarThree}></div>
+          </h1>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Header/Menu.js
@@ -0,0 +1,72 @@
+import AniLink from "gatsby-plugin-transition-link/AniLink";
+import React from "react";
+import menuStyles from "./menu.module.scss";
+
+function Menu(props) {
+  const { closeMenu, menu } = props;
+  return (
+    <div
+      id={menuStyles.container}
+      className={menu ? menuStyles.show : menuStyles.hide}
+    >
+      <ul>
+        <li className={menuStyles.link}>
+          <AniLink
+            className={menuStyles.link}
+            cover
+            direction="right"
+            bg="#1b1924"
+            to="/portfolio"
+            style={{ textDecoration: "none" }}
+          >
+            .portfolio
+            <div className={menuStyles.line}></div>
+          </AniLink>
+        </li>
+        <div className={menuStyles.divideLine}></div>
+        <li onClick={() => alert("Coming Soon!")} className={menuStyles.link}>
+          {/* <AniLink
+            className={menuStyles.link}
+            cover
+            direction="right"
+            bg="#1b1924"
+            to="/music"
+            style={{ textDecoration: "none" }}
+          > */}
+          .music
+          <div className={menuStyles.line}></div>
+          {/* </AniLink> */}
+        </li>
+        <div className={menuStyles.divideLine}></div>
+        <li className={menuStyles.link}>
+          <AniLink
+            className={menuStyles.link}
+            cover
+            direction="right"
+            bg="#1b1924"
+            style={{ textDecoration: "none" }}
+            to="/about"
+          >
+            .about
+            <div className={menuStyles.line}></div>
+          </AniLink>
+        </li>
+        <div className={menuStyles.divideLine}></div>
+        <li className={menuStyles.link}>
+          <AniLink
+            className={menuStyles.link}
+            cover
+            direction="right"
+            to="/contact"
+            style={{ textDecoration: "none" }}
+          >
+            .contact
+            <div className={menuStyles.line}></div>
+          </AniLink>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Menu;
