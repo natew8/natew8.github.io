54aa2aa0dc4ce0f893d31aa0ad6f923bf4baca19
Nathan Waite <natew821@gmail.com>
2021-02-22 10:19:40 -0700

    Added Images to folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,14 @@ module.exports = {
     "gatsby-plugin-scroll-reveal",
     "gatsby-plugin-transition-link",
     "gatsby-plugin-smoothscroll",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: `${__dirname}/src/images`,
+      },
+    },
   ],
 };
